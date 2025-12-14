b1970a3e1d82dd6a3300944012d59f3626bbc50a
root <root@srv1195560.hstgr.cloud>
2025-12-14 19:35:55 +0000

    Fix CSRF trusted origins and proxy SSL header

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://72.61.78.228",
+]
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # Application definition
 
