c1ad16b9f747a719bf0abd059ef1ddbeb45086ba
root <root@srv1195560.hstgr.cloud>
2025-12-14 21:09:27 +0000

    urls actulizadas

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),  # Esta línea es la que incluye las URLs de autenticación
     path("", include("inventory.urls")),
 ]
 
